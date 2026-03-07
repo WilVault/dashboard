fd1b2a5a270a729050eec068cba1b178fc621f9c
Ubuntu <ubuntu@ip-172-31-20-204.ap-southeast-1.compute.internal>
2026-03-07 05:24:41 +0000

    haha

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -51,7 +51,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>GREYAN'S WORLD</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
