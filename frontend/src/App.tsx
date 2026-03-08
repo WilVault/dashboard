b9f287053cf1e7409d5e8922409cdea1cdec4f76
Wilson Gonzales <wdotgonzales@gmail.com>
2026-03-08 13:36:04 +0800

    refactor: modify text

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -51,7 +51,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>GREYAN'S WORLD</h1>
+      <h1>TEST 123</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
