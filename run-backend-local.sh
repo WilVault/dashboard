688e59d2f261107c8d4cb793cf9d08025bafff4e
Wilson Gonzales <wdotgonzales@gmail.com>
2026-03-23 13:43:22 +0800

    fix virtual env bug

--- run-backend-local.sh
+++ run-backend-local.sh
@@ -2,8 +2,12 @@
 
 python load_config.py local
 cd backend
-rm -rf venv
-python -m venv venv
+
+# Only create venv if it doesn't exist
+if [ ! -d "venv" ]; then
+  python -m venv venv
+fi
+
 source venv/bin/activate
 pip install -r requirements.txt
 python run.py
